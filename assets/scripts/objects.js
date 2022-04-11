fd739ea677e2596cbf4057158eb9d10f89b2b0e0
jaba777 <tskhovreba777@gmail.com>
2022-04-11 05:22:56 +0100

    first commit

---
+++ assets/scripts/objects.js
@@ -0,0 +1,35 @@
+const addMovieBtn=document.getElementById('add-movie-btn');
+const searchBtn= document.getElementById('search-btn');
+
+const movies=[];
+
+const renderMovie=()=>{
+    const movieList=document.getElementById('movie-list');
+}
+
+const addMovieHandler=()=>{
+    const title = document.getElementById('title').value;
+    const extraName=document.getElementById('extra-name').value;
+    const extraValue=document.getElementById('extra-value').value;
+
+    if(title.trim()==='' ||
+       extraName.trim()==='' ||
+       extraValue.trim()===''){
+           return;
+       }
+
+       const newMovie={
+           info: {
+               title,
+               [extraName]:extraValue
+           },
+           id: Math.random()
+       };
+
+       movies.push(newMovie);
+       console.log(movies);
+
+       
+};
+
+addMovieBtn.addEventListener('click',addMovieHandler);
